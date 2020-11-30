6a613233c2d48845f58261745a6aab7c25696012
SebastianJL <johannes.lade@yahoo.com>
2020-11-30 23:17:13 +0100

    Prepare repo for day 1.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent_of_code_2020
+
+See more info on <https://adventofcode.com/2020>.
--- example/advent_of_code_2020_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:advent_of_code_2020/advent_of_code_2020.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
