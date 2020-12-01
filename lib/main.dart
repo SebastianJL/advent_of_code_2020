fe06d253703456b33c2fe65f7f038f40ec60c40f
SebastianJL <johannes.lade@yahoo.com>
2020-12-01 18:57:29 +0100

    Day 01, ex 2.

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,25 @@ void main() {
       numbers_file.readAsLinesSync().map((e) => int.parse(e)).toList();
   var number1;
   var number2;
+  var number3;
 
   outer:
   for (var i=0; i<numbers.length; i++) {
     number1 = numbers[i];
     for (var j=i+1; j<numbers.length; j++) {
       number2 = numbers[j];
-      if (number1 + number2 == 2020) {
-        break outer;
+      for (var k=i+1; k<numbers.length; k++) {
+        number3 = numbers[k];
+        if (number1 + number2 + number3 == 2020) {
+          break outer;
+        }
       }
     }
   }
 
   print(number1);
   print(number2);
-  print(number1 + number2);
-  print(number1 * number2);
+  print(number3);
+  print(number1 + number2 + number3);
+  print(number1 * number2 * number3);
 }
