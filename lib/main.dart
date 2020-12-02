448f86eb1ee0af9474ece7acc943b519d53eb173
SebastianJL <johannes.lade@yahoo.com>
2020-12-02 23:25:07 +0100

    Day 02, ex 2, async version.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:advent_of_code_2020/src/operators.dart';
 
 void main() {
   var password_file = File('lib/passwords.txt');
-  var n_valid_passwords = password_file
-      .readAsLinesSync()
+  password_file
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(LineSplitter())
       .map((e) => e.split(RegExp('[- ]')))
-      .map((e) => is_valid(
-          int.parse(e[0])-1, int.parse(e[1])-1, e[2].substring(0, 1), e[3]))
-      .reduce((sum, e) => sum + e);
-
-  print(n_valid_passwords);
+      .map((e) => isValid(
+          int.parse(e[0]) - 1, int.parse(e[1]) - 1, e[2].substring(0, 1), e[3]))
+      .reduce((sum, e) => sum + e)
+      .then(print);
 }
 
-int is_valid(int index1, int index2 , String character, String password) {
+int isValid(int index1, int index2, String character, String password) {
   var valid = true;
   valid = xor(character == password[index1], character == password[index2]);
   return valid ? 1 : 0;
