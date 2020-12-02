17ab2146cab1e439a7b9585b77dbb46b3429a394
SebastianJL <johannes.lade@yahoo.com>
2020-12-02 20:41:35 +0100

    Day 02, ex 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
 import 'dart:io';
 
+import 'package:advent_of_code_2020/src/operators.dart';
+
 void main() {
   var password_file = File('lib/passwords.txt');
   var n_valid_passwords = password_file
       .readAsLinesSync()
       .map((e) => e.split(RegExp('[- ]')))
       .map((e) => is_valid(
-          int.parse(e[0]), int.parse(e[1]), e[2].substring(0, 1), e[3]))
+          int.parse(e[0])-1, int.parse(e[1])-1, e[2].substring(0, 1), e[3]))
       .reduce((sum, e) => sum + e);
 
   print(n_valid_passwords);
 }
 
-int is_valid(int min, int max, String character, String password) {
-  var counter = 0;
+int is_valid(int index1, int index2 , String character, String password) {
   var valid = true;
-  for (var c in password.split('')) {
-    if (c == character) counter++;
-  }
-  if (counter < min) valid = false;
-  if (counter > max) valid = false;
+  valid = xor(character == password[index1], character == password[index2]);
   return valid ? 1 : 0;
 }
