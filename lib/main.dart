83c7aa55e17e9559ba5854a178e398fde14c4241
SebastianJL <johannes.lade@yahoo.com>
2020-12-03 19:44:53 +0100

    Day 03, ex 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
-import 'dart:convert';
 import 'dart:io';
 
-import 'package:advent_of_code_2020/src/operators.dart';
-
 void main() {
-  var password_file = File('lib/passwords.txt');
-  password_file
-      .openRead()
-      .transform(utf8.decoder)
-      .transform(LineSplitter())
-      .map((e) => e.split(RegExp('[- ]')))
-      .map((e) => isValid(
-          int.parse(e[0]) - 1, int.parse(e[1]) - 1, e[2].substring(0, 1), e[3]))
-      .reduce((sum, e) => sum + e)
-      .then(print);
-}
+  var tree_map = File('lib/tree_map.txt')
+      .readAsLinesSync()
+      .map((e) => e.split(''))
+      .toList();
 
-int isValid(int index1, int index2, String character, String password) {
-  var valid = true;
-  valid = xor(character == password[index1], character == password[index2]);
-  return valid ? 1 : 0;
+  var nRows = tree_map.length;
+  var nColumns = tree_map[0].length;
+  var nTrees = 0;
+
+  var c = 0;
+  for (var r = 0; r < nRows; r++) {
+    if (tree_map[r][c] == '#') nTrees++;
+    c = (c+3) % nColumns;
+  }
+  print(nTrees);
 }
