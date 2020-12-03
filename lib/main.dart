0d341ba329a8607bd9b755a127c946af5a06a63f
SebastianJL <johannes.lade@yahoo.com>
2020-12-03 20:01:44 +0100

    Day 03, ex 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'dart:io';
 
 void main() {
-  var tree_map = File('lib/tree_map.txt')
+  var treeMap = File('lib/tree_map.txt')
       .readAsLinesSync()
       .map((e) => e.split(''))
       .toList();
 
-  var nRows = tree_map.length;
-  var nColumns = tree_map[0].length;
+  var total = 1;
+  for (var slope in [
+    [1, 1],
+    [1, 3],
+    [1, 5],
+    [1, 7],
+    [2, 1]
+  ]) {
+    var nTrees = countTrees(treeMap, slope[0], slope[1]);
+    print(nTrees);
+    total *= nTrees;
+  }
+  print(total);
+}
+
+int countTrees(List<List<String>> treeMap, int down, int right) {
+  var nRows = treeMap.length;
+  var nColumns = treeMap[0].length;
   var nTrees = 0;
 
   var c = 0;
-  for (var r = 0; r < nRows; r++) {
-    if (tree_map[r][c] == '#') nTrees++;
-    c = (c+3) % nColumns;
+  for (var r = 0; r < nRows; r += down) {
+    if (treeMap[r][c] == '#') nTrees++;
+    c = (c + right) % nColumns;
   }
-  print(nTrees);
+  return nTrees;
 }
