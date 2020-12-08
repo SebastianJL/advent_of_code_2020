7434c1d8c17dbd84759867537b4d4876aa1ff6ed
SebastianJL <johannes.lade@yahoo.com>
2020-12-09 00:56:06 +0100

    Day 06, ex 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,12 @@
 import 'dart:io';
 
 void main() {
-  var boardingPasses = File('lib/boarding_passes.txt').readAsLinesSync();
+  var nAnswers = File('lib/answer_sheet.txt')
+      .readAsStringSync()
+      .split('\n\n')
+      .map((e) => e.replaceAll('\n', '').split(''))
+      .map((e) => Set<String>.from(e))
+      .fold(0, (int previousValue, element) => previousValue + element.length);
 
-  var seatIDs = boardingPasses.map((e) => decodeSeatId(e)).toList();
-  seatIDs.sort();
-
-  for (var i=0; i<seatIDs.length-1; i++) {
-    if (seatIDs[i+1] - seatIDs[i] == 2) {
-      print(seatIDs[i]+1);
-    };
-  }
-}
-
-int decodeSeatId(String seatString) {
-  var row = int.parse(
-      seatString
-          .substring(0, 7)
-          .split('')
-          .map((letter) => letter == 'B' ? 1 : 0)
-          .join(),
-      radix: 2);
-
-  var col = int.parse(
-      seatString
-          .substring(7)
-          .split('')
-          .map((letter) => letter == 'R' ? 1 : 0)
-          .join(),
-      radix: 2);
-
-  return row * 8 + col;
+  print(nAnswers);
 }
