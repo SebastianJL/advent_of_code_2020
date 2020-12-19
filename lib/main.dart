1e7483ff4d0141c5cf7fd43bf966c1e69e87f53d
SebastianJL <johannes.lade@yahoo.com>
2020-12-19 09:59:33 +0100

    Day 09, ex 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'dart:collection';
 import 'dart:io';
 
-void main() {
-  var program =
-      File('lib/data.xmas').readAsLinesSync().map((e) => int.parse(e)).toList();
-  var n = 25;
-  var previousNumbers = Queue.of(program.sublist(0, n));
-  print(previousNumbers);
+import 'dart:math';
 
-  outer:
-  for (var number in program.sublist(n)) {
-    for (var previous in previousNumbers) {
-      if (previousNumbers.contains(number - previous)) {
-        previousNumbers.removeFirst();
-        previousNumbers.addLast(number);
-        continue outer;
-      }
+void main() {
+  var data =
+      File('lib/data.xmas').readAsLinesSync().map((e) => int.parse(e)).toList();
+  var n = 400480901;
+  var sum = 0;
+  var contiguousRange = Queue<int>();
+
+  for (var number in data) {
+    sum += number;
+    contiguousRange.addLast(number);
+
+    while (sum > n) {
+      sum -= contiguousRange.removeFirst();
     }
-    print(number);
-    break;
+    if (sum == n) {
+      break;
+    }
   }
+  print(sum);
+  var minimum = contiguousRange.reduce(min);
+  var maximum = contiguousRange.reduce(max);
+  print(minimum + maximum);
 }
