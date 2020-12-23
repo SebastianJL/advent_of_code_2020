5be1719a695ef5a9832e545e94d072c5202060a8
SebastianJL <johannes.lade@yahoo.com>
2020-12-23 22:26:42 +0100

    Day 11, ex 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,78 @@
+// @dart=2.9
 import 'dart:io';
+import 'dart:math';
 
-import 'package:advent_of_code_2020/src/operators.dart';
+import 'package:collection/collection.dart';
+
+const noSeat = '.';
+const free = 'L';
+const occupied = '#';
 
 void main() {
-  var chargers = File('lib/chargers.txt')
+  var seatLayout = File('lib/seat_layout.txt')
       .readAsLinesSync()
-      .map((e) => int.parse(e))
+      .map((e) => e.split(''))
       .toList();
+  var seatLayoutCopy =
+      List.filled(seatLayout.length, List.filled(seatLayout[0].length, ''));
+  Function deepEquals = const DeepCollectionEquality().equals;
 
-  chargers.sort();
-  chargers = [0, ...chargers, chargers[chargers.length - 1] + 3];
-  print(chargerCombinations(chargers));
+  do {
+    deepCopy(seatLayout, seatLayoutCopy);
+
+    // matrixPrint(seatLayout);
+    // print('---------------------------');
+
+    for (var row = 0; row < seatLayout.length; row++) {
+      for (var col = 0; col < seatLayout[0].length; col++) {
+        if (seatLayoutCopy[row][col] == noSeat) continue;
+
+        var n = countOccupiedAdjacentSeats(row, col, seatLayoutCopy);
+
+        if (n == 0) seatLayout[row][col] = occupied;
+        if (n >= 4) seatLayout[row][col] = free;
+      }
+    }
+  } while (!deepEquals(seatLayout, seatLayoutCopy));
+
+  // matrixPrint(seatLayoutCopy);
+  print(countOccupiedSeats(seatLayout));
 }
 
-int chargerCombinations(List<int> chargers) {
-  var combinations = List.filled(chargers.length, 0);
-  combinations.last = 1;
+int countOccupiedAdjacentSeats(
+    int row, int col, List<List<String>> seatLayout) {
+  var n = 0;
+  var iMin = max(row - 1, 0);
+  var iMax = min(row + 2, seatLayout.length);
+  var jMin = max(col - 1, 0);
+  var jMax = min(col + 2, seatLayout[0].length);
 
-  for (var i = chargers.length - 2; i >= 0; i--) {
-    var first = chargers[i];
-    var possibleNext =
-        chargers.skip(i).takeWhile((value) => first >= value - 3).length;
-    combinations[i] = combinations.skip(i).take(possibleNext).reduce(add);
+  for (var i = iMin; i < iMax; i++) {
+    for (var j = jMin; j < jMax; j++) {
+      n += (seatLayout[i][j] == occupied) ? 1 : 0;
+    }
   }
-  return combinations[0];
+  if (seatLayout[row][col] == occupied) n -= 1;
+  return n;
+}
+
+int countOccupiedSeats(List<List<String>> seatLayout) {
+  return seatLayout
+      .expand((element) => element)
+      .where((element) => element == occupied)
+      .length;
+}
+
+void matrixPrint<E>(List<List<E>> matrix) {
+  for (var row in matrix) {
+    stdout.writeAll(row);
+    print('');
+  }
+  print('');
+}
+
+void deepCopy(List<List<String>> origin, List<List<String>> target) {
+  for (var i = 0; i < origin.length; i++) {
+    target[i] = List.from(origin[i]);
+  }
 }
