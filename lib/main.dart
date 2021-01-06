6b7d8941edad30729d89d822693eb898904b5794
SebastianJL <johannes.lade@yahoo.com>
2021-01-06 01:14:37 +0100

    Day 14, ex 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,48 @@
-import 'dart:collection';
 import 'dart:io';
 
+import 'package:advent_of_code_2020/src/operators.dart';
+
 void main() {
-  var notes = File('lib/notes.txt')
-      .readAsLinesSync()[0]
-      .split(',')
-      .map((e) => int.tryParse(e))
-      .toList()
-      .asMap();
+  var instructions = File('lib/instructions.txt')
+      .readAsLinesSync()
+      .map((e) => e.split(' = '))
+      .toList();
 
-  var offsetToInterval = SplayTreeMap<int, int>.fromIterable(
-      notes.entries.where((entry) => entry.value != null),
-      key: (e) => e.key,
-      value: (e) => e.value);
+  var memory = <int, int>{};
+  late BitMask mask;
 
-  var incr = offsetToInterval.remove(offsetToInterval.firstKey())!;
-  var value = 0;
+  for (var instruction in instructions) {
+    var operation = instruction[0];
+    var operand = instruction[1];
 
-  for (var entry in offsetToInterval.entries) {
-    var offset = entry.key;
-    var interval = entry.value;
-    while ((value + offset) % interval != 0) {
-      value += incr;
+    if (operation == 'mask') {
+      mask = BitMask(operand);
+    } else {
+      var match = RegExp(r'\[(\d+)\]').firstMatch(operation)!;
+      var addr = int.parse(match.group(1)!);
+      var value = int.parse(operand);
+      memory[addr] = mask.apply(value);
     }
-    incr = lcm(incr, interval);
+  }
+  var sum = memory.values.reduce(add);
+  print(sum);
+}
+
+class BitMask {
+  late final int zeroes;
+  late final int ones;
+
+  BitMask(String bitMask) {
+    var onesBinaryString =
+        bitMask.replaceAll('X', '0').split('').map((e) => int.parse(e)).join();
+    var zeroesBinaryString =
+        bitMask.replaceAll('X', '1').split('').map((e) => int.parse(e)).join();
+
+    ones = int.parse(onesBinaryString, radix: 2);
+    zeroes = int.parse(zeroesBinaryString, radix: 2);
   }
 
-  print(value);
-}
-
-/// Returns greatest common divisor of [a] and [b].
-int gcd(int a, int b) {
-  return a.gcd(b);
-}
-
-/// Returns least common multiple of [a] and [b].
-int lcm(int a, int b) {
-  return (a * b).abs() ~/ gcd(a, b);
+  int apply(int value) {
+    return (value | ones) & zeroes;
+  }
 }
