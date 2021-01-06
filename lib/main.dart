91aed069a360818e8979594d8caf7ae4e6b18d82
SebastianJL <johannes.lade@yahoo.com>
2021-01-06 02:26:18 +0100

    Day 14, ex 2.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:advent_of_code_2020/src/operators.dart';
 
@@ -20,8 +21,10 @@ void main() {
     } else {
       var match = RegExp(r'\[(\d+)\]').firstMatch(operation)!;
       var addr = int.parse(match.group(1)!);
-      var value = int.parse(operand);
-      memory[addr] = mask.apply(value);
+      var floatingAddresses = mask.apply(addr);
+      for (var floatingAddr in floatingAddresses) {
+        memory[floatingAddr] = int.parse(operand);
+      }
     }
   }
   var sum = memory.values.reduce(add);
@@ -29,20 +32,36 @@ void main() {
 }
 
 class BitMask {
-  late final int zeroes;
   late final int ones;
+  late final List<int> xPositions;
+  late final int length;
 
   BitMask(String bitMask) {
     var onesBinaryString =
         bitMask.replaceAll('X', '0').split('').map((e) => int.parse(e)).join();
-    var zeroesBinaryString =
-        bitMask.replaceAll('X', '1').split('').map((e) => int.parse(e)).join();
 
     ones = int.parse(onesBinaryString, radix: 2);
-    zeroes = int.parse(zeroesBinaryString, radix: 2);
+
+    xPositions =
+        RegExp('X').allMatches(bitMask).map((match) => match.start).toList();
+
+    length = bitMask.length;
   }
 
-  int apply(int value) {
-    return (value | ones) & zeroes;
+  List<int> apply(int value) {
+    var maskedValue =
+        ((value | ones)).toRadixString(2).padLeft(length, '0').split('');
+    var floatingValues = <int>[];
+    var nDigits = xPositions.length;
+    var max = pow(2, nDigits) as int;
+
+    for (var i = 0; i < max; i++) {
+      var bits = i.toRadixString(2).padLeft(nDigits, '0');
+      for (var j = 0; j < bits.length; j++) {
+        maskedValue[xPositions[j]] = bits[j];
+      }
+      floatingValues.add(int.parse(maskedValue.join(), radix: 2));
+    }
+    return floatingValues;
   }
 }
